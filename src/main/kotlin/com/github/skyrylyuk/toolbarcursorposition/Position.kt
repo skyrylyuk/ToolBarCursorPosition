700200b7d7cd07307798161fdd95d9c4d4a8dd40
Sergii Kyrylyuk <s.kyrylyuk@thedigital.gov.ua>
2025-04-21 11:40:29 +0300

    Initial commit

---
+++ src/main/kotlin/com/github/skyrylyuk/toolbarcursorposition/Position.kt
@@ -0,0 +1,9 @@
+package com.github.skyrylyuk.toolbarcursorposition
+
+import com.intellij.openapi.wm.StatusBarWidget
+import com.intellij.openapi.wm.StatusBarWidgetFactory
+import com.intellij.openapi.wm.Too
+
+
+class Position {
+}
